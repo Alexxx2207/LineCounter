cf3051c5bdf70c0e6544bc9b612b01d5334fea12
Kristian Gergov <kristian.gergov1@gmail.com>
2022-07-09 17:32:23 +0300

    Initial Commit.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace LineCounter 
+{
+    class Program 
+    {
+        public static void Main(string[] args) 
+        {
+            string loc = "";
+            if (args.Length < 1) 
+            {
+                Console.WriteLine("No arguments. Scanning current folder.");
+                loc = ".";
+            }
+            else loc = args[0];
+            var files = Directory.GetFileSystemEntries(loc);
+            int lines = 0;
+            for (int i = 0; i < files.Length; i++)
+            {
+                lines += CountLines(files[i]);
+            }
+            Console.WriteLine($"Lines: {lines}");
+        }
+        private static int CountLines(string location) 
+        {
+            int lines = 0;
+            if (Directory.Exists(location)) 
+            {
+                foreach (var entry in Directory.GetFileSystemEntries(location)) 
+                {
+                    lines += CountLines(entry);
+                }
+            }
+            if (!location.EndsWith(".cs") || !location.EndsWith(".java") || !location.EndsWith(".txt") || !location.EndsWith(".json") || !location.EndsWith(".c") || !location.EndsWith(".cpp")) 
+                return lines;
+            lines += File.ReadAllLines(location).Count();
+            return lines;
+        }
+    }
+}
